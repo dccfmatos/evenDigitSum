9dcf6ffa66135916daf87702c97166143d0d5cdb
dccfmatos <d.catarina.c.f@gmail.com>
2019-05-20 18:51:47 +0100

    Initial commit

---
+++ src/EvenDigitSum.java
@@ -0,0 +1,22 @@
+public class EvenDigitSum {
+    ublic static int getEvenDigitSum(int number){
+
+        if (number < 0){
+            return -1;
+        } else {
+            int sum = 0;
+
+            while ( number > 0){
+                int digit = number % 10;
+
+                if (digit % 2 == 0){
+                    sum += digit;
+                }
+
+                number = number / 10;
+            }
+            return sum;
+        }
+
+    }
+}
